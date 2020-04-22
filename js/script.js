cd9b2591f9eb42ac4d927d5edc48c93c76d77600
Philipe Anderson De Campos <philipeacampos@hotmail.com>
2020-04-21 23:31:54 -0300

    inserida as classes arrow para eventlistener

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 var menuDoc = document.getElementsByClassName('link-nav-arrow')[0]
 var listSecond = document.querySelector('.list-nav-second')
 
+
+// var iconMenu = document.getElementById('icon-menu');
+// var menu = document.getElementById('menu')
+// iconMenu.hideMenu('click', function(){
+//     menu.hidden('menu')
+// });
+
+
+
 menuDoc.addEventListener('click', function(){
 
     listSecond.classList.toggle('hide')
